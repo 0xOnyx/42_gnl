f41de9eea3e5b90cb3a0090e5aa4ba25aa4d6433
onyx <info@jeremy.best>
2022-08-11 14:36:54 +0200

    first try

---
+++ get_next_line.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerdos-s <jerdos-s@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 17:45:14 by jerdos-s          #+#    #+#             */
+/*   Updated: 2022/08/11 14:26:39 by jerdos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char const *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char	*dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	len = 0;
+	i = 0;
+	while (len < size && dst[len] != '\0')
+		len++;
+	while (len + i + 1 < size && src[i] != '\0')
+	{
+		dst[len + i] = src[i];
+		i++;
+	}
+	if (len + i < size)
+		dst[len + i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (len + i);
+}
+
+int	ft_read_from_buff(char **current_buff, int fd)
+{
+	char	*res;
+	char	*buff;
+	int	len;
+	
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (0);
+	res = *current_buff;
+	while (ft_strchr(res, '\n') || len <= 0)
+	{
+		len = read(fd, buff, BUFFER_SIZE);
+		if (len < 0)
+		{
+			free(buff);
+			return (0);
+		}
+		buff[len] = '\0';
+		res = ft_strjoin(res, buff);
+		free(*current_buff);
+		*current_buff = res;
+	}
+	return (1);
+}
+
+char	*ft_init_new_buff(char *buff)
+{
+	char	*end;
+
+	end = ft_strchr(buff, '\n');
+	if (!end)
+		end = ft_strchr(buff, '\0');
+	if (!end)
+	{
+		free(buff);
+		return (NULL);
+	}
+	end = ft_strjoin(end + 1, NULL);
+	free(buff);
+	return (end);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*current_buff;
+	char		*current_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if(!ft_read_from_buff(&current_buff, fd))
+	{
+		free(current_buff);
+		return (NULL);
+	}
+	current_line = ft_get_line(current_buff);
+	if (!current_line)
+		return (NULL);
+	current_buff = ft_init_new_buff(current_buff);
+	if (current_buff)
+		return (NULL);
+	return (current_line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerdos-s <jerdos-s@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 18:28:18 by jerdos-s          #+#    #+#             */
+/*   Updated: 2022/08/11 14:28:06 by jerdos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+
+
+char	*get_next_line(int fd);
+int	ft_read_from_buff(char **current_buff, int fd);
+char	*ft_get_line(char *current_buff);
+char	*ft_init_new_buff(char *current_buff);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_substr(char const *s, size_t len);
+size_t	ft_strlen(char const *str);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+
+#endif
